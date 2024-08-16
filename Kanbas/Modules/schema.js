02107e0cd8ee7deb3fde224eb4988cce182e4f1a
Neil Israni <114356760+NeilIsrani@users.noreply.github.com>
2024-08-16 06:43:12 +0530

    Update schema.js

---
+++ Kanbas/Modules/schema.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const lessonSchema = new mongoose.Schema({
+  _id: { type: String, required: true, unique: true },  // Unique identifier for each lesson
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  module: { type: String, ref: "Module", required: true }  // References the module to which the lesson belongs
+});
+
+const moduleSchema = new mongoose.Schema({
+  _id: { type: String, required: true, unique: true },  
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  course: { type: String, ref: "Course", required: true },  
+  lessons: [lessonSchema]  
+}, { collection: "modules" });
+
+export default mongoose.model("Module", moduleSchema);
